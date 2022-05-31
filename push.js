075452d37ae6d62e9f92eab4aadd0915864b40b2
VVIvanov1 <ivanovv.tca@gmail.com>
2022-05-31 13:25:21 +0600

    fixed

--- push.js
+++ push.js
@@ -14,13 +14,13 @@ push.setVapidDetails(
 
 let sub = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/dDEmR8f0RAI:APA91bHs7xs05pK0rOguY6aGJXjdtr93zFlDTesHDb31yg89dwytqz6KbcS1pkcZfX8Z4Yy9Q50OZH15aH3fYZBGvdeDdRK4zNeiOgC0-9AjGtCDnA6I3cJEhUv6aFfcJ83uaFtKWXjG",
+    "https://fcm.googleapis.com/fcm/send/cVFM8Y8N9dw:APA91bG-nIZJS8VZyn_7uGbGAiGMam8x_kym6Wx-7S3fAA8nt2OOA_lQmau6kVWTbg6DfTLyn4FpzPhs9c3cq1DVoaIhjTk7HmNzYzvRTTv3P7nl-7BgEaCg9hw82S0QAMwzpLzTn_2t",
   expirationTime: null,
   keys: {
     p256dh:
-      "BM2SlnLoR9OIGkEEVcvngGlEnHe1lsUrtj2eLQjxrhorB9gAV-u2Gzc-_PPZSDeOszcJbImJaBRAgljqknjkLN0",
-    auth: "y79nfF9QpDWQjPLYQGZrqA",
+      "BOyndrhvvRP3ra5CzVCtfd3bTB8vEIy9CpP36E4lCHing333xdkwADMKB7ukFCK-ri0CnGbzuVHEeysPZ7cshGk",
+    auth: "hb7QqZ3c4JJGObVRXnQmoQ",
   },
 };
 
-push.sendNotification(sub, "test message");
+push.sendNotification(sub, "KMON!");
