08c1bbe72f4860809ebe956743cff8df41d01b89
VVIvanov1 <ivanovv.tca@gmail.com>
2022-05-31 12:52:32 +0600

    fixed

--- push.js
+++ push.js
@@ -12,4 +12,15 @@ push.setVapidDetails(
   vapidkeys.privateKey
 );
 
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/f32iX3u8NTk:APA91bHD-c8hpw8lZ-ByFlywZtg8N37wPXDisGm3EnlWxUlh4G38aIwQ-uWXFrCKIuvois_LRXvbFWEjRx8_-owIZwKCaKWAtJ6asHhsijdWc5nW3WvfN15kIcwGUundadNaVz7v7vZr",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BNIxG626jwAlgUiiifWvC_M6BwMJDt8eUvJDMffk46cbw8Ho5JXZPF0UHH3YKI-7TonOw2ThJ0Y2_8_u3YFa__s",
+    auth: "4zkVZAsyG-RRLXfQ_HJx_Q",
+  },
+};
+
 push.sendNotification(sub, "test message");
